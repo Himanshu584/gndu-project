dcc9b0b0b70851de7c648378309274af03fbe084
Himanshu Sharma <Hsharma98765@gmail.com>
2021-04-28 16:17:17 +0530

    production setup

--- gndu_project/settings.py
+++ gndu_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zisob#%yk(fwl@5ncpojf^p@(n+=sah*0ok7$^dj8s(obnl_ae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gndu-registration.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = [BASE_DIR / "staticfiles",]
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn gndu_project.wsgi --log-file -
